2020471cf87bf3e49f043df00c30efd2ef5211c3
ruivergani <ruiverganineto@gmail.com>
2022-12-28 22:33:08 +0000

    modal

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -8,3 +8,22 @@ btnOutline.addEventListener('mouseover', () => {
 btnOutline.addEventListener('mouseleave', () => {
     refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-btn.svg');
 });
+
+
+// Configure the modal
+const btnOpenModal = document.querySelector('.js-open-modal'); 
+const btnCloseModal = document.querySelector('.js-close');
+
+btnOpenModal.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  let html = document.documentElement;
+   // add the class to the html (not to the specific element)
+   html.classList.add('show-modal'); // selecting 
+})
+
+btnCloseModal.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  let html = document.documentElement;
+   // add the class to the html (not to the specific element)
+   html.classList.remove('show-modal'); // selecting 
+})
