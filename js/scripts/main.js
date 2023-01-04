67d8162a7d7df2831acc6fb78ca00263566cba11
ruivergani <ruiverganineto@gmail.com>
2023-01-04 11:37:49 +0000

    Header JS and CSS done

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,29 +1,54 @@
-
 // Hover Buttons
-const btnOutline = document.getElementById('btn-outline');
-const refreshImage = document.getElementById('refresh');
-btnOutline.addEventListener('mouseover', () => {
-        refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-red.svg');
-});
-btnOutline.addEventListener('mouseleave', () => {
-    refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-btn.svg');
-});
-
+// const btnOutline = document.getElementById('btn-outline');
+// const refreshImage = document.getElementById('refresh');
+// btnOutline.addEventListener('mouseover', () => {
+//         refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-red.svg');
+// });
+// btnOutline.addEventListener('mouseleave', () => {
+//     refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-btn.svg');
+// });
 
 // Configure the modal
-const btnOpenModal = document.querySelector('.js-open-modal'); 
-const btnCloseModal = document.querySelector('.js-close');
+// const btnOpenModal = document.querySelector('.js-open-modal'); 
+// const btnCloseModal = document.querySelector('.js-close');
 
-btnOpenModal.addEventListener('click', (Event) => {
-  Event.preventDefault();
-  let html = document.documentElement;
-   // add the class to the html (not to the specific element)
-   html.classList.add('show-modal'); // selecting 
-})
+// btnOpenModal.addEventListener('click', (Event) => {
+//   Event.preventDefault();
+//   let html = document.documentElement;
+//    // add the class to the html (not to the specific element)
+//    html.classList.add('show-modal'); // selecting 
+// })
 
-btnCloseModal.addEventListener('click', (Event) => {
-  Event.preventDefault();
-  let html = document.documentElement;
-   // add the class to the html (not to the specific element)
-   html.classList.remove('show-modal'); // selecting 
-})
+// btnCloseModal.addEventListener('click', (Event) => {
+//   Event.preventDefault();
+//   let html = document.documentElement;
+//    // add the class to the html (not to the specific element)
+//    html.classList.remove('show-modal'); // selecting 
+// })
+
+// Button Scroll Top
+// const btnScrollTop = document.getElementById("js-btn-scroll-top");
+// btnScrollTop.addEventListener('click', () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth"
+//   });
+// });
+
+// Add class active to menu button
+// const menuButton = document.getElementById('js-menu-button');
+// menuButton.addEventListener('click', () => {
+//   menuButton.classList.toggle('is-active'); // add the CSS configuration on this button
+//   document.documentElement.classList.toggle('menu-opened');
+// });
+
+// Menu Header Change >20 scroll
+const header = document.getElementById('js-header')
+function fixedMenu () {
+    if(window.pageYOffset > 60) {
+        header.classList.add('changeHeight');
+    } else {
+        header.classList.remove('changeHeight');
+    }
+}
+document.addEventListener('scroll', fixedMenu);
