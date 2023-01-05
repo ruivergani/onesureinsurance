bd641914cad68bef4db065337acd13e505ad55b8
ruivergani <ruiverganineto@gmail.com>
2023-01-05 17:15:50 +0000

    icon refresh fix

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,13 +1,3 @@
-// Hover Buttons
-const btnOutline = document.getElementById('btn-outline');
-const refreshImage = document.getElementById('refresh');
-btnOutline.addEventListener('mouseover', () => {
-        refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-red.svg');
-});
-btnOutline.addEventListener('mouseleave', () => {
-    refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-btn.svg');
-});
-
 // Configure the modal
 // const btnOpenModal = document.querySelector('.js-open-modal'); 
 // const btnCloseModal = document.querySelector('.js-close');
