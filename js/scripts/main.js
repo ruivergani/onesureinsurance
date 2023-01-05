4dbdf85d53daee4dee3687df39b567be29cbb20c
ruivergani <ruiverganineto@gmail.com>
2023-01-05 14:10:17 +0000

    Section all types insurance HTML done

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,12 +1,12 @@
 // Hover Buttons
-// const btnOutline = document.getElementById('btn-outline');
-// const refreshImage = document.getElementById('refresh');
-// btnOutline.addEventListener('mouseover', () => {
-//         refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-red.svg');
-// });
-// btnOutline.addEventListener('mouseleave', () => {
-//     refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-btn.svg');
-// });
+const btnOutline = document.getElementById('btn-outline');
+const refreshImage = document.getElementById('refresh');
+btnOutline.addEventListener('mouseover', () => {
+        refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-red.svg');
+});
+btnOutline.addEventListener('mouseleave', () => {
+    refreshImage.setAttribute('src', '../../assets/icons/icon-refresh-btn.svg');
+});
 
 // Configure the modal
 // const btnOpenModal = document.querySelector('.js-open-modal'); 
