f8df1df13158e450227ddeb3c8723f44bc0a4b4e
ruivergani <ruiverganineto@gmail.com>
2023-01-09 12:11:03 +0000

    performance fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,75 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create(); // create local server
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
-
-// Compilando o sass, adicionando autoprefixed e dando refresh na pagina
-function compilaSass() {
-  return gulp.src('scss/*.scss')
-  .pipe(sass())
-  .pipe(autoprefixer({
-    overrideBrowserslist: ['last 2 versions'],
-    cascade: false,
-  }))
-  .pipe(gulp.dest('css/'))
-  .pipe(browserSync.stream());
+// Compile Sass Function
+function compilaSass(){
+    return gulp.src('scss/*.scss') // get all files from folder scss
+    .pipe(sass({outputStyle : 'compressed'})) //minified css
+    .pipe(autoprefixer({ // create autoprefixer
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false,
+    })) 
+    .pipe(gulp.dest('css/')) // destination folder
+    .pipe(browserSync.stream()); // inject css into the page
 }
-// tarefa do sass
-gulp.task('sass', compilaSass);
+gulp.task('sass', compilaSass); // task (need a name)  default for all execute
 
-function pluginsCSS() {
-  return gulp.src('css/lib/*.css')
-  .pipe(concat('plugins.css'))
-  .pipe(gulp.dest('css/'))
-  .pipe(browserSync.stream())
+// Concat all plugins CSS
+function pluginsCSS(){
+    return gulp.src('css/lib/*.css')
+    .pipe(concat('plugins.css'))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream())
 }
-
 gulp.task('plugincss', pluginsCSS);
 
-function gulpJs() {
-  return gulp.src('js/scripts/*.js')
-  .pipe(concat('all.js'))
-  .pipe(babel({
-      presets: ['@babel/env']
-  }))
-  .pipe(uglify())
-  .pipe(gulp.dest('js/'))
-  .pipe(browserSync.stream());
+// Concat all JS 
+function gulpJS(){
+    return gulp.src('js/scripts/*.js')
+    .pipe(concat('all.js')) // put all files in the scripts folder to one single file
+    .pipe(babel({ // compile modern JS
+        presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.stream()); // inject js into the page
 }
-gulp.task('alljs', gulpJs);
+gulp.task('allJS', gulpJS);
 
-function pluginsJs() {
-  return gulp
-  .src(['./js/lib/aos.min.js','./js/lib/swiper.min.js'])
-  .pipe(concat('plugins.js'))
-  .pipe(gulp.dest('js/'))
-  .pipe(browserSync.stream())
+// Concat all plugins JS
+function pluginsJS(){
+    return gulp
+    .src(['./js/lib/aos.min.js', './js/lib/swiper.min.js']) // name of files
+    .pipe(concat('plugins.js')) // all these files will be going into plugins.js
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.stream())
 }
+gulp.task('pluginjs', pluginsJS);
 
-gulp.task('pluginjs', pluginsJs);
-
-// funcao do browsersync
-function browser() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    }
-  })
+// Browser Function
+function browser(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
 }
-//tarefa do browsersync
 gulp.task('browser-sync', browser);
 
-//funcao do watch para alteracoes em scss e html
-function watch() {
-  gulp.watch('scss/*.scss', compilaSass);
-
-  gulp.watch('css/lib/*.css', pluginsCSS);
-
-  gulp.watch('*.html').on('change', browserSync.reload);
-
-  gulp.watch('js/scripts/*js', gulpJs);
-
-  gulp.watch('js/lib/*.js', pluginsJs);
+// Watch Function
+function watch(){
+    gulp.watch('scss/*.scss', compilaSass); // gulp.series('name-of-task') or use parrallel
+    gulp.watch('css/lib/*.css', pluginsCSS);
+    gulp.watch('*.html').on('change', browserSync.reload); // refresh html when changes made
+    gulp.watch('js/scripts/*.js', gulpJS);
+    gulp.watch('js/lib/*.js', pluginsJS);
 }
-//tarefa do watch
 gulp.task('watch', watch);
 
-// tarefas default que executa o watch e o browsersync
-gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'plugincss', 'alljs', 'pluginjs'));
+// Gulp default
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass','plugincss', 'allJS', 'pluginjs'));
