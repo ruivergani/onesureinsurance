67b26ca6ce94c046ee3d63713cce890577081ee0
ruivergani <ruiverganineto@gmail.com>
2023-01-11 17:20:51 +0000

    Changes main js file

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,49 +1,50 @@
 // Configure the modal
-// const btnOpenModal = document.querySelector('.js-open-modal'); 
+// const btnOpenModal = document.querySelector('.js-open-modal');
 // const btnCloseModal = document.querySelector('.js-close');
 
 // btnOpenModal.addEventListener('click', (Event) => {
 //   Event.preventDefault();
 //   let html = document.documentElement;
 //    // add the class to the html (not to the specific element)
-//    html.classList.add('show-modal'); // selecting 
+//    html.classList.add('show-modal'); // selecting
 // })
 
 // btnCloseModal.addEventListener('click', (Event) => {
 //   Event.preventDefault();
 //   let html = document.documentElement;
 //    // add the class to the html (not to the specific element)
-//    html.classList.remove('show-modal'); // selecting 
+//    html.classList.remove('show-modal'); // selecting
 // })
 
-// Script for the Tabs
-const allFilters = document.querySelectorAll('.js-nav-product li a');
-const tabPane = document.querySelectorAll('.text-content');
-if(allFilters && tabPane){
+// Script for the Tab Navigation
+const allFilters = document.querySelectorAll(".js-nav-product li a");
+const tabPane = document.querySelectorAll(".text-content");
+if (allFilters && tabPane) {
   // get the click of the nav for the tabs
   allFilters.forEach((filter, index) =>
-    filter.addEventListener('click', (Event) => {
+    filter.addEventListener("click", (Event) => {
       Event.preventDefault();
       // remove active from all others
-      allFilters.forEach(item => {
-        item.classList.remove('active');
-      })
-      tabPane.forEach(item => {
-        item.classList.remove('active');
-      })
+      allFilters.forEach((item) => {
+        item.classList.remove("active");
+      });
+      tabPane.forEach((item) => {
+        item.classList.remove("active");
+      });
       // add active when click
-      tabPane[index].classList.add('active'); // link the index from the nav to the tab
-      filter.classList.add('active'); // add active to the nav buttons
+      tabPane[index].classList.add("active"); // link the index from the nav to the tab
+      filter.classList.add("active"); // add active to the nav buttons
     })
   );
 }
 
 // Script for the FAQs open
-const question = document.querySelectorAll('.js-question'); // all cards question
-if(question){ // Check if the question exists or not
-  question.forEach(function(event){
-    event.addEventListener('click', () => {
-      event.parentElement.classList.toggle('active');
+const question = document.querySelectorAll(".js-question"); // all cards question
+if (question) {
+  // Check if the question exists or not
+  question.forEach(function (event) {
+    event.addEventListener("click", () => {
+      event.parentElement.classList.toggle("active");
     });
   });
 }
@@ -56,20 +57,20 @@ var swiper = new Swiper(".slide-logos", {
     el: ".s-logos .container .swiper-pagination",
     clickable: true,
   },
-  breakpoints:{
-    320 : {
-        slidesPerView: 1,
-        spaceBetween: 60,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 60,
     },
     768: {
       slidesPerView: 2.3,
       spaceBetween: 30,
     },
     1200: {
-        slidesPerView: 3,
-        spaceBetween: 90,
-    }
-  }
+      slidesPerView: 3,
+      spaceBetween: 90,
+    },
+  },
 });
 // Swiper Slide Products Page
 var slide_product = new Swiper(".slide-product", {
@@ -79,7 +80,7 @@ var slide_product = new Swiper(".slide-product", {
     el: ".s-slide-product .container .top .ctrl-slide .swiper-pagination",
     clickable: true,
   },
-  // navigation section 
+  // navigation section
   navigation: {
     nextEl: ".s-slide-product .top .btn-next",
     prevEl: ".s-slide-product .top .btn-prev",
@@ -95,48 +96,47 @@ var slide_product = new Swiper(".slide-product", {
     },
     // tablet
     560: {
-      slidesPerView: 1.5
+      slidesPerView: 1.5,
     },
     768: {
-      slidesPerView: 2.1
+      slidesPerView: 2.1,
     },
     991: {
-      slidesPerView: 2.5
+      slidesPerView: 2.5,
     },
     1150: {
-      slidesPerView: 3.5
+      slidesPerView: 3.5,
     },
     1200: {
       slidesPerView: 4,
-      spaceBetween: 32
+      spaceBetween: 32,
     },
-  }
+  },
 });
 
 // Button Scroll Top
 const btnScrollTop = document.getElementById("js-btn-scroll-top");
-btnScrollTop.addEventListener('click', () => {
+btnScrollTop.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 });
 
 // Add class active to menu button
-const menuButton = document.getElementById('js-menu-button');
-menuButton.addEventListener('click', () => {
-  menuButton.classList.toggle('is-active'); // add the CSS configuration on this button
-  document.documentElement.classList.toggle('menu-opened');
+const menuButton = document.getElementById("js-menu-button");
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("is-active"); // add the CSS configuration on this button
+  document.documentElement.classList.toggle("menu-opened");
 });
 
 // Menu Header Change >20 scroll
-const header = document.getElementById('js-header')
-function fixedMenu () {
-    if(window.pageYOffset > 60) {
-        header.classList.add('changeHeight');
-    } else {
-        header.classList.remove('changeHeight');
-    }
+const header = document.getElementById("js-header");
+function fixedMenu() {
+  if (window.pageYOffset > 60) {
+    header.classList.add("changeHeight");
+  } else {
+    header.classList.remove("changeHeight");
+  }
 }
-document.addEventListener('scroll', fixedMenu);
-
+document.addEventListener("scroll", fixedMenu);
