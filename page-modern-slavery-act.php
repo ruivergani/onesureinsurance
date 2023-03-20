d2c2ad8c87d93d7c81e0bc44f5242e75e335a95e
ruivergani <ruiverganineto@gmail.com>
2023-03-20 11:21:22 +0000

    Page Modern Slavery Act done

---
+++ page-modern-slavery-act.php
@@ -0,0 +1,50 @@
+<?php
+  // Template name: modern-act-policy
+?>
+
+<!-- Include Header -->
+<?php get_header(); ?>
+
+<!-- Loop in the template -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
+
+ <!-- Section Breadcrumbs -->
+ <?php breadcrumbs(); ?>
+
+ <section class="s-title">
+    <div class="container">
+      <h1><?php wp_title('') ?></h1>
+    </div>
+ </section>
+
+ <section class="s-page-text">
+  <div class="container">
+    <div class="left-page-text">
+      <!-- Blog Structure based on WordPress -->
+      <div class="content-post">
+        <?php the_content(); ?>
+      </div>
+    </div>
+    <div class="right-page-text">
+      <!-- Table of Content -->
+      <div class="table-of-content">
+        <h5 class="table-of-content-title">Table of Content</h5>
+        <!-- Top Navigation -->
+        <ul class="js-nav-template-text">
+        </ul>
+        <a href="https://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" target="_blank">
+          <button>
+            <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-share-arrow.svg" alt="icon share arrow" title="icon share arrow">
+            <span>Share</span>
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+ </section>
+
+<!-- End loop -->
+<?php endwhile; else: endif; ?>
+
+<!-- Include Footer -->
+<?php get_footer(); ?>
