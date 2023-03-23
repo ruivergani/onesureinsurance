54544cdfefdec5b68727c44c31e5776f6bb837a1
ruivergani <ruiverganineto@gmail.com>
2023-03-23 11:07:20 +0000

    CSS section hero support

--- page-support.php
+++ page-support.php
@@ -11,10 +11,12 @@
 <!-- Section Hero -->
 <section class="s-hero-support">
   <div class="container">
+    <div class="title-support">
       <h1>Need help?</h1>
       <p>Don’t worry - we’re here for you! Seach for help by typing your question in the box, or scroll down to pick the topic you need.</p>
-      <!-- Section Include Search Code Blog -->
-      <?php include(TEMPLATEPATH .'/includes/section-search-blog.php') ?> 
+    </div>
+    <!-- Section Include Search Code Blog -->
+    <?php include(TEMPLATEPATH .'/includes/section-search-blog.php') ?> 
   </div>
 </section>
 
