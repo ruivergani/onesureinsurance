51e5d7c4b402f453bfcda398a32c1e7868802985
ruivergani <ruiverganineto@gmail.com>
2023-03-23 09:35:49 +0000

    Section hero html page support

--- page-support.php
+++ page-support.php
@@ -8,7 +8,17 @@
 <!-- Loop in the template -->
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
 
-  <h1>Page Support</h1>
+<!-- Section Hero -->
+<section class="s-hero-support">
+  <div class="container">
+      <h1>Need help?</h1>
+      <p>Don’t worry - we’re here for you! Seach for help by typing your question in the box, or scroll down to pick the topic you need.</p>
+      <!-- Section Include Search Code Blog -->
+      <?php include(TEMPLATEPATH .'/includes/section-search-blog.php') ?> 
+  </div>
+</section>
+
+
 
 <!-- End loop -->
 <?php endwhile; else: endif; ?>
