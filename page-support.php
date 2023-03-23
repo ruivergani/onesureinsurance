a8fa7aa2a8875b1bd749c80b540f3586c4949ce4
ruivergani <ruiverganineto@gmail.com>
2023-03-23 11:27:24 +0000

    Section cards css desktop done

--- page-support.php
+++ page-support.php
@@ -22,66 +22,64 @@
 
 <section class="s-rounded-cards">
   <div class="container">
-    <div class="rounded-cards-top">
-      <ul>
-        <li>
-          <div class="cards-rounded">
-            <div class="circle-rounded">
-              <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-car-rounded.svg" alt="car rounded icon" title="car rounded icon">
-            </div>
+    <ul>
+      <li>
+        <div class="cards-rounded">
+          <div class="circle-rounded">
+            <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-car-rounded.svg" alt="car rounded icon" title="car rounded icon">
           </div>
-          <a href="">Car Insurance</a>
-        </li>
-        <li>
-          <div class="cards-rounded">
-            <div class="circle-rounded">
-              <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-lorry-rounded.svg" alt="lorry rounded icon" title="lorry rounded icon">
-            </div>
+        </div>
+        <a href="">Car Insurance</a>
+      </li>
+      <li>
+        <div class="cards-rounded">
+          <div class="circle-rounded">
+            <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-lorry-rounded.svg" alt="lorry rounded icon" title="lorry rounded icon">
           </div>
-          <a href="">HGV & Lorry Insurance</a>
-        </li>
-        <li>
-          <div class="cards-rounded">
-            <div class="circle-rounded">
-              <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-scooter-rounded.svg" alt="scooter rounded icon" title="scooter rounded icon">
-            </div>
+        </div>
+        <a href="">HGV & Lorry Insurance</a>
+      </li>
+      <li>
+        <div class="cards-rounded">
+          <div class="circle-rounded">
+            <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-scooter-rounded.svg" alt="scooter rounded icon" title="scooter rounded icon">
           </div>
-          <a href="">Scooter Insurance</a>
-        </li>
-        <li>
-          <div class="cards-rounded">
-            <div class="circle-rounded">
-              <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-van-rounded.svg" alt="van rounded icon" title="van rounded icon">
-            </div>
+        </div>
+        <a href="">Scooter Insurance</a>
+      </li>
+      <li>
+        <div class="cards-rounded">
+          <div class="circle-rounded">
+            <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-van-rounded.svg" alt="van rounded icon" title="van rounded icon">
           </div>
-          <a href="">Van Insurance</a>
-        </li>
-        <li>
-          <div class="cards-rounded">
-            <div class="circle-rounded">
-              <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-house-rounded.svg" alt="home rounded icon" title="home rounded icon">
-            </div>
+        </div>
+        <a href="">Van Insurance</a>
+      </li>
+      <li>
+        <div class="cards-rounded">
+          <div class="circle-rounded">
+            <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-house-rounded.svg" alt="home rounded icon" title="home rounded icon">
           </div>
-          <a href="">Home Insurance</a>
-        </li>
-        <li>
-          <div class="cards-rounded">
-            <div class="circle-rounded">
-              <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-motorcycle-rounded.svg" alt="motorbike rounded icon" title="motorbike rounded icon">
-            </div>
+        </div>
+        <a href="">Home Insurance</a>
+      </li>
+      <li>
+        <div class="cards-rounded">
+          <div class="circle-rounded">
+            <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-motorcycle-rounded.svg" alt="motorbike rounded icon" title="motorbike rounded icon">
           </div>
-          <a href="">Motorbike Insurance</a>
-        </li>
-        <li>
-          <div class="cards-rounded">
-            <div class="circle-rounded">
-              <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-caravan-rounded.svg" alt="caravan rounded icon" title="caravan rounded icon">
-            </div>
+        </div>
+        <a href="">Motorbike Insurance</a>
+      </li>
+      <li>
+        <div class="cards-rounded">
+          <div class="circle-rounded">
+            <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-caravan-rounded.svg" alt="caravan rounded icon" title="caravan rounded icon">
           </div>
-          <a href="">Caravan Insurance</a>
-        </li>
-      </ul>
-    </div>
+        </div>
+        <a href="">Caravan Insurance</a>
+      </li>
+    </ul>
   </div>
 </section>
 
