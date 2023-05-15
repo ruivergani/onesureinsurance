6781ff12eff2854abd8107ef626b0084ca58c889
ruivergani <ruiverganineto@gmail.com>
2023-05-15 15:49:18 +0100

    Section CTA and Policy done

--- page-claims.php
+++ page-claims.php
@@ -56,7 +56,47 @@
     </div>
   </section>
 
+  <!-- === SECTION CONTENT FAQs === -->
+  <section>
+    <div class="container">
 
+    </div>
+  </section>
+
+  <!-- === Section CTA Desktop === -->
+  <div class="s-newsletter s-claims-newsletter">
+    <div class="container">
+      <a href="#" class="large-cta" data-aos="zoom-in" data-aos-once="true">
+        <div class="text">
+            <span>We Are Confident Of Our Pricing That We Challenge You To Beat Our Guarantee</span>
+            <h3>We Are Britains Cheapest Insurer* </h3>
+        </div>
+        <button class="btn btn-primary">
+          <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-arrow-forward.svg" alt="icon forward arrow white" title="icon forward arrow white" loading="lazy">
+          get a quote
+        </button>
+      </a>
+    </div>
+  </div>
+
+  <!-- === Section Policy === -->
+  <section class="s-policy-support">
+    <div class="container">
+      <img src="<?php echo get_template_directory_uri()?>/assets/icons/icon-invoice-support-page.svg" alt="icon policy" title="icon policy" class="policy">
+      <h2>Log In To Your Account</h2>
+      <ul>
+        <!-- Repeater Slide Cards --> 
+        <?php if( have_rows('') ): while ( have_rows('') ) : the_row(); ?>
+          <li>
+            <p>Donec vitae interdum nisl. Proin tincidunt malesuada viverra. Fusce porttitor lorem ut est cursus, et sollicitudin sapien pretium. Mauris placerat eros massa, quis semper mauris faucibus et.</p>
+          </li>
+        <?php endwhile; else : endif;?>
+      </ul>
+      <a class="btn btn-primary" href="">
+        Log in to your account
+      </a>
+    </div>
+  </section>
 
 
 <!-- End loop -->
