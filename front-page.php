11fc7442ea7b5d14a46c07472cebaf78527c8ade
ruivergani <ruiverganineto@gmail.com>
2023-06-01 09:56:34 +0100

    created secondary feature content and fixed text

--- front-page.php
+++ front-page.php
@@ -54,7 +54,7 @@
   <section class="s-trustpilot">
     <div class="container">
       <!-- TrustBox widget - Carousel --> 
-      <div class="trustpilot-widget" data-locale="en-US" data-template-id="53aa8912dec7e10d38f59f36" data-businessunit-id="55e56b2c0000ff000582d042" data-style-height="140px" data-style-width="100%" data-theme="light" data-stars="1,2,3,4,5" data-review-languages="en"> 
+      <div class="trustpilot-widget" data-locale="en-US" data-template-id="53aa8912dec7e10d38f59f36" data-businessunit-id="55e56b2c0000ff000582d042" data-style-height="140px" data-style-width="100%" data-theme="light" data-stars="4,5" data-review-languages="en"> 
         <a href="https://www.trustpilot.com/review/onesureinsurance.co.uk" target="_blank" rel="noopener">Trustpilot</a> 
       </div> 
       <!-- End TrustBox widget -->
--- page-van-insurance.php
+++ page-van-insurance.php
@@ -164,6 +164,21 @@
     </div>
   </section>
 
+  <!-- Section Featured Secondary Content -->
+  <section class="s-featured-content">
+    <div class="container">
+       <div class="s-featured-content__right" data-aos="fade-right" data-aos-once="true">
+        <img src="<?php the_field('title_featured_content_secondary_van_insurance') ?>" alt="">
+       </div>
+      <div class="s-featured-content__left" data-aos="fade-left" data-aos-once="true">
+        <h4><?php the_field('title_featured_content_secondary_van_insurance') ?></h4>
+        <div class="s-featured-content__text">
+          <?php the_field('description_featured_content_secondary_van_insurance') ?>
+        </div>
+      </div>
+    </div>
+  </section>
+
   <!-- Section Column Content -->
   <section class="s-column-content">
     <div class="container">
