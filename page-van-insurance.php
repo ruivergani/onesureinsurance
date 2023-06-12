8718fd58ad5f42f8263404dea960be78d7ac98c3
ruivergani <ruiverganineto@gmail.com>
2023-06-12 10:56:35 +0100

    section logos insurance

--- page-van-insurance.php
+++ page-van-insurance.php
@@ -89,8 +89,34 @@
     </div>
   </section>
 
-  <!-- Section Accreditations  -->
-  <?php include(TEMPLATEPATH .'/includes/section-company-logos.php') ?>
+  <section class="s-logos-insurance" id="s-logos">
+    <div class="container">
+      <div class="title" data-aos="fade-right" data-aos-once="true">
+        <h6>Have a look</h6>
+        <h5>Our Accreditations</h5>
+      </div>
+      <!-- Swipper -->
+      <div class="slide-logos" data-aos="fade-left" data-aos-once="true">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="card-image">
+              <img src="<?php echo get_template_directory_uri()?>/assets/logo-company01.svg" alt="logo company" title="logo company" loading="lazy">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="card-image">
+              <img src="<?php echo get_template_directory_uri()?>/assets/logo-company03.svg" alt="logo company" title="logo company" loading="lazy">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="card-image">
+              <img src="<?php echo get_template_directory_uri()?>/assets/logo-company04.svg" alt="logo company" title="logo company" loading="lazy">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
   <!-- Table of Contents -->
   <section class="s-table-contents">
