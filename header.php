7e3a6c49e6d0eddd8467c569f1ef9860ef072fa9
ruivergani <ruiverganineto@gmail.com>
2023-07-17 10:27:34 +0100

    changes functions php for performance

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Meta Tag -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Jquery Link -->
   <script src="<?php echo get_template_directory_uri()?>/js/scripts/jquery-3.6.4.min.js"></script>
   <!-- TrustBox script --> 
   <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script> 
   <!-- End TrustBox script -->
-  <!-- Meta Tag -->
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Favicon -->
   <link rel="shortcut icon" type="image/svg" href="<?php echo get_template_directory_uri()?>/favicon.png">
   <!-- Font Awesome Links -->
